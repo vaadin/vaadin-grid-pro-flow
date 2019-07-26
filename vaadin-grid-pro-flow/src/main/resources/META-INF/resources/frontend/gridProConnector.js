20b5f4300a922664f3b99e8b0838f284cd7542c8
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2019-07-26 12:52:22 +0300

    Improve error handling in connector: wrap with try catch

--- vaadin-grid-pro-flow/src/main/resources/META-INF/resources/frontend/gridProConnector.js
+++ vaadin-grid-pro-flow/src/main/resources/META-INF/resources/frontend/gridProConnector.js
@@ -1,25 +1,41 @@
+// Error handling functions
+const tryCatchWrapper = function(originalFunction) {
+    return function() {
+        try {
+            originalFunction.apply(this, arguments);
+        } catch (error) {
+            logError(error.message);
+        }
+    }
+}
+
+function logError(message) {
+    console.error("There seems to be an error in the GridPro:\n" + message + "\n" +
+       "Please submit an issue to https://github.com/vaadin/vaadin-grid-pro-flow/issues/new!");
+}
+
 window.Vaadin.Flow.gridProConnector = {
-  setEditModeRenderer: function(column, component) {
-      column.editModeRenderer = function(root) {
+  setEditModeRenderer: tryCatchWrapper(function(column, component) {
+      column.editModeRenderer = tryCatchWrapper(function(root) {
           root.appendChild(component);
           this._grid._cancelStopEdit();
           component.focus();
-      };
+      });
 
-      column._setEditorValue = function(editor, value) {
+      column._setEditorValue = tryCatchWrapper(function(editor, value) {
           // Not needed in case of custom editor as value is set on server-side.
           // Overridden in order to avoid blinking of the cell content.
-      }
+      })
 
-      column._getEditorValue = function(editor) {
+      column._getEditorValue = tryCatchWrapper(function(editor) {
           // Not needed in case of custom editor as value is set on server-side.
           // Overridden in order to avoid blinking of the cell content.
           return;
-      }
-  },
+      })
+  }),
 
-  patchEditModeRenderer: function(column) {
-      column.__editModeRenderer = function(root, column, rowData) {
+  patchEditModeRenderer: tryCatchWrapper(function(column) {
+      column.__editModeRenderer = tryCatchWrapper(function(root, column, rowData) {
           const cell = root.assignedSlot.parentNode;
           const grid = column._grid;
 
@@ -34,6 +50,6 @@ window.Vaadin.Flow.gridProConnector = {
               <${tagName}></${tagName}>
             `;
           }
-      };
-  }
+      });
+  })
 };
